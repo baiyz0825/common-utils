9b9800c9baf49c64b60c6fb69a84ec9e87536d22
yizhuobai <yizhuobai@tencent.com>
2023-09-14 16:24:34 +0800

    add ci

--- docker/build-and-push.sh
+++ docker/build-and-push.sh
@@ -4,15 +4,16 @@
 # @Version:
 # @Author: BaiYiZhuo
 # @Date: 2023-09-14 15:40:23
-# @LastEditTime: 2023-09-14 16:22:43
+# @LastEditTime: 2023-09-14 16:24:01
 ###
 for dir in */; do
     cd "$dir"
     echo "将要处理  $(pwd)"
     dir=${dir%*/}
-    echo "Building Docker image: $dir"
+    echo "Building Docker image path : $dir"
     tag=ghcr.io/baiyz0825/common-utils/$dir:latest
-    docker build -t $tag -f $dir/Dockerfile .
+    echo "Building Docker image Tag is: $tag"
+    docker build -t $tag -f ./Dockerfile .
     if [ $? -eq 0 ]; then
         echo "Docker image $dir built successfully"
         echo "Pushing Docker image: $dir"
