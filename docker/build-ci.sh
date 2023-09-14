23ef89cd79f2351f20343589d025432782bb2b90
yizhuobai <yizhuobai@tencent.com>
2023-09-14 16:03:17 +0800

    add ci

---
+++ docker/build-ci.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+###
+# @Description:
+# @Version:
+# @Author: BaiYiZhuo
+# @Date: 2023-09-14 15:40:23
+# @LastEditTime: 2023-09-14 15:58:01
+###
+cd docker || exit
+for dir in */; do
+    dir=${dir%*/}
+    echo "Building Docker image: $dir"
+    tag=ghcr.io/baiyz0825/common-utils/$dir:latest
+    docker build -t $tag -f $dir/Dockerfile .
+    if [ $? -eq 0 ]; then
+        echo "Docker image $dir built successfully"
+        echo "Pushing Docker image: $dir"
+        docker push $tag
+        if [ $? -eq 0 ]; then
+            echo "Docker image $tag pushed successfully"
+        else
+            echo "Failed to push Docker image $tag"
+        fi
+    else
+        echo "Failed to build Docker image $tag"
+    fi
+done
