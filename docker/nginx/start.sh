1665d3ed54818198a04cb8d341b69e8ee8ec9048
baiyz0825 <byz0825@outlook.com>
2024-01-11 00:43:55 +0800

    Update start.sh

--- docker/nginx/start.sh
+++ docker/nginx/start.sh
@@ -12,8 +12,8 @@
 echo '开始检查证书'
 if [ ! -f "/etc/nginx/cert/$CERT_NAME.crt" ] || [ ! -f "/etc/nginx/cert/$CERT_NAME.key" ]; then
     echo "证书文件不存在,开始申请证书..."
-    /root/.acme.sh/acme.sh --issue --dns $DNS_PROVIDER -d $DOMAIN_NAME --register-account $REGISTER_EMAIL
-    /root/.acme.sh/acme.sh --installcert -d $DOMAIN_NAME --key-file /etc/nginx/cert/$CERT_NAME.key --fullchain-file /etc/nginx/cert/$CERT_NAME.crt --reloadcmd "service nginx force-reload"
+    /root/.acme.sh/acme.sh --issue --debug --dns $DNS_PROVIDER -d $DOMAIN_NAME --register-account -m $REGISTER_EMAIL
+    /root/.acme.sh/acme.sh --installcert --debug -d $DOMAIN_NAME --key-file /etc/nginx/cert/$CERT_NAME.key --fullchain-file /etc/nginx/cert/$CERT_NAME.crt --reloadcmd "service nginx force-reload"
 else
     echo "证书文件已经安装了,不会在进行安装!"
 fi
